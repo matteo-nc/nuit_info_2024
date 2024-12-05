abc43fe6ca16dabf6f084ee725d0b87d0e3e0897
Hafez Ben-Abdillah MADI <hafezbenabdillah.madi@gmail.com>
2024-12-06 00:43:51 +0100

    add: popup

---
+++ frontend/src/components/Popup.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  defineProps<{
+    visible: boolean
+  }>();
+</script>
+
+<template>
+  <dialog :open='visible' class='overflow-auto p-10 rounded-xl border-2 border-black'>
+    <slot></slot>
+  </dialog>
+</template>
+
+<style scoped>
+
+</style>
