5e5a2ad687562f063aa10a353d8565c7be7cd42e
SamuelPouyade <samuel.pouyade@outlook.fr>
2024-12-05 23:18:54 +0100

    Complétion de l'header
    Ce commit permet d'ajouter les icônes pour chaques menus.

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -3,11 +3,26 @@
 </script>
 
 <template>
-  <header class="bg-rose-50 w-full py-5 ">
-    <nav class="flex flex-row items-center gap-24">
-      <RouterLink to="/">Accueil</RouterLink>
-      <RouterLink to="/test">Test-vous</RouterLink>
-      <RouterLink to="/credits">Crédits</RouterLink>
+  <header class="bg-blue-500 w-full">
+    <nav class="flex flex-row items-center gap-16">
+        <RouterLink class="hover:bg-cyan-300 p-5" to="/">
+          <span class="flex flex-row items-center gap-2">
+            <img src="../assets/principe.svg" class="logo size-10 inline" alt="Vite logo" />
+            <p>Accueil</p>
+          </span>
+        </RouterLink>
+      <RouterLink class="hover:bg-cyan-300 p-5" to="/test">
+        <span class="flex flex-row items-center gap-2">
+            <img src="../assets/game.svg" class="logo size-10 inline" alt="Vite logo" />
+            <p>Testez-vous</p>
+        </span>
+      </RouterLink>
+      <RouterLink class="hover:bg-cyan-300 p-5" to="/credits">
+        <span class="flex flex-row items-center gap-2">
+            <img src="../assets/contact.svg" class="logo size-10 inline" alt="Vite logo" />
+            <p>Crédits</p>
+        </span>
+      </RouterLink>
     </nav>
   </header>
 </template>
