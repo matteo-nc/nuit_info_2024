8b1fbb4f849ef64764aeed07ac9881f886812e35
Florian Rajoye <florian.rajoye@etu.unilim.fr>
2024-12-06 06:13:25 +0100

    dessin beau

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,21 +1,76 @@
-<script setup lang="ts">
+<script>
+import Popup from "@/components/Popup.vue";
 
-import Popup from "./Popup.vue";
+export default {
+  name: 'home',
+  components: {Popup},
+  props: {
+    msg: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      humain: null,
+      ocean: null,
+      switch: null,
+      humainVisible: true,
+      humainStyle: {
+        position: 'relative',
+        height: '60rem',
+        width: '42rem',
+      },
+      oceanStyle: {
+        position: 'relative',
+        height: '60rem',
+        width: '42rem',
+      }
+    };
+  },
+  methods: {
+    toggleOcean() {
+      const switchElement = this.$refs.switch;
+      if (!switchElement) {
+        return;
+      }
+
+      let checked = switchElement.checked;
+      this.humainVisible = !checked;
+    }
+  },
+};
 </script>
 
 <template>
   Home
+  <!-- Premier popup -->
   <Popup :visible="true" class="left-1/2 top-[200px] bg-gray-300">
     <p>Tototo</p>
     <p>WAWA</p>
   </Popup>
+  <label class="inline-flex items-center cursor-pointer">
+    <input ref="switch" type="checkbox" value="" class="sr-only peer" @change="toggleOcean">
+    <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+    <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Mode Océan</span>
+  </label>
+  <div v-if="humainVisible"  ref="humain" class="mx-auto" :style="humainStyle">
+    <img alt="peauHumain" src="../../public/fondHumain.png" class="object-cover absolute inset-0 w-full h-full z-0">
+    <img alt="veines" src="../../public/veines.png" class="object-cover absolute inset-0 w-full h-full z-10">
+    <img alt="poumonhumain" src="../../public/poumonsHumain.png" class=" object-cover absolute inset-0 w-full h-full z-20">
+    <img alt="coeurhumain" src="../../public/coeurHumain.png" class=" object-cover absolute inset-0 w-full h-full z-30">
+  </div>
 
+  <div v-else ref="ocean" class="relative mx-auto" :style="oceanStyle">
+    <img alt="peauocean" src="../../public/fondOcean.png" class="object-cover absolute inset-0 w-full h-full z-0">
+    <img alt="veines" src="../../public/veines.png" class="object-cover absolute inset-0 w-full h-full z-10">
+    <img alt="poumonsOcean" src="../../public/poumonsOcean.png" class="object-cover absolute inset-0 w-full h-full z-20">
+    <img alt="coeurOcean" src="../../public/coeurOcean.png" class="object-cover absolute inset-0 w-full h-full z-30">
+  </div>
+
+  <!-- Deuxième popup -->
   <Popup :visible="true" class="right-1/2 top-[200px] bg-gray-300">
     <p>Tototo</p>
     <p>WAWA</p>
   </Popup>
 </template>
-
-<style scoped>
-
-</style>
