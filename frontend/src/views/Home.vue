54d76c77be04cd5fa43350f8d1074b0dd676cdd5
Florian Rajoye <95242110+TheProNic@users.noreply.github.com>
2024-12-06 06:49:04 +0100

    description (#12)

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -16,6 +16,7 @@
         ocean: null,
         switch: null,
         humainVisible: true,
+        info: 'Cliquez sur une partie du corps pour en savoir plus',
         humainStyle: {
           position: 'relative',
           height: '60rem',
@@ -37,6 +38,19 @@
 
         let checked = switchElement.checked;
         this.humainVisible = !checked;
+      },
+      getInfo() {
+        const infos = [
+          {img: 'sang', desc: 'Le sang comme le peuvent les courrants marin, permettent le transport des nutriments nécessaires à la vie' },
+          {img: 'cellules', desc: 'Les cellules étant très nombreuses et diverses peuvent être comparées à la biodiversité des océans' },
+          {img: 'jambes', desc: "Les jambes et les vagues s'adaptent au différents terrains" },
+          {img: 'poumons', desc: "Les poumons tout comme les phytoplanctons permettent de respirer" },
+          {img: 'cerveau', desc: "Le cerveau et les abysses sont des endroits mystérieux et inexplorés" },
+        ];
+
+        let randomIndex = Math.floor(Math.random() * infos.length);
+
+        this.info = infos[randomIndex].desc;
       }
     },
   };
@@ -44,34 +58,28 @@
 
 <template>
   Home
-  <!-- Premier popup -->
-  <Popup :visible="true" class="left-1/2 top-[200px] bg-gray-300">
-    <p>Tototo</p>
-    <p>WAWA</p>
-  </Popup>
   <label class="inline-flex items-center cursor-pointer">
     <input ref="switch" type="checkbox" value="" class="sr-only peer" @change="toggleOcean">
     <div class="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
     <span class="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">Mode Océan</span>
   </label>
   <div v-if="humainVisible"  ref="humain" class="mx-auto" :style="humainStyle">
-    <img alt="peauHumain" src="../../public/fondHumain.png" class="object-cover absolute inset-0 w-full h-full z-0">
-    <img alt="veines" src="../../public/veines.png" class="object-cover absolute inset-0 w-full h-full z-10">
-    <img alt="poumonhumain" src="../../public/poumonsHumain.png" class=" object-cover absolute inset-0 w-full h-full z-20">
-    <img alt="coeurhumain" src="../../public/coeurHumain.png" class=" object-cover absolute inset-0 w-full h-full z-30">
+    <img alt="peauHumain" src="../../public/fondHumain.png" class="object-cover absolute inset-0 w-full h-full z-0" @click="getInfo">
+    <img alt="veines" src="../../public/veines.png" class="object-cover absolute inset-0 w-full h-full z-10" @click="getInfo">
+    <img alt="poumonhumain" src="../../public/poumonsHumain.png" class=" object-cover absolute inset-0 w-full h-full z-20" @click="getInfo">
+    <img alt="coeurhumain" src="../../public/coeurHumain.png" class=" object-cover absolute inset-0 w-full h-full z-30" @click="getInfo">
   </div>
 
   <div v-else ref="ocean" class="relative mx-auto" :style="oceanStyle">
-    <img alt="peauocean" src="../../public/fondOcean.png" class="object-cover absolute inset-0 w-full h-full z-0">
-    <img alt="veines" src="../../public/veines.png" class="object-cover absolute inset-0 w-full h-full z-10">
-    <img alt="poumonsOcean" src="../../public/poumonsOcean.png" class="object-cover absolute inset-0 w-full h-full z-20">
-    <img alt="coeurOcean" src="../../public/coeurOcean.png" class="object-cover absolute inset-0 w-full h-full z-30">
+    <img alt="peauocean" src="../../public/fondOcean.png" class="object-cover absolute inset-0 w-full h-full z-0" @click="getInfo">
+    <img alt="veines" src="../../public/veines.png" class="object-cover absolute inset-0 w-full h-full z-10" @click="getInfo">
+    <img alt="poumonsOcean" src="../../public/poumonsOcean.png" class="object-cover absolute inset-0 w-full h-full z-20" @click="getInfo">
+    <img alt="coeurOcean" src="../../public/coeurOcean.png" class="object-cover absolute inset-0 w-full h-full z-30" @click="getInfo">
   </div>
 
-  <!-- Deuxième popup -->
-  <Popup :visible="true" class="right-1/2 top-[200px] bg-gray-300">
-    <p>Tototo</p>
-    <p>WAWA</p>
+  <!-- Premier popup -->
+  <Popup :visible="true" class="left-1/2 top-[200px] bg-gray-300">
+    <p class="text-balance">{{ info }}</p>
   </Popup>
 </template>
 
