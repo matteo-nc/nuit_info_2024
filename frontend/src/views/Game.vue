1c9cd94d2791c3aa2e9a550a87b0ed0a9e0b01df
SamuelPouyade <samuel.pouyade@outlook.fr>
2024-12-06 05:36:50 +0100

    Réglement conflits

--- frontend/src/views/Game.vue
+++ frontend/src/views/Game.vue
@@ -1,13 +1,44 @@
 <script setup lang="ts">
-  import RunAwayButton from "../components/RunAwayButton.vue";
-  import RunAwayInput from "../components/RunAwayInput.vue";
+
+import Question from "../components/Question.vue";
+import {QuestionModel} from "../game.model.ts";
+import {ref} from "vue";
+
+const question = ref<QuestionModel>(null);
+
+const reset = ref(false);
+
+function getRandomInt(max: number) {
+  return Math.floor(Math.random() * max);
+}
+
+
+
+const reinitialiserQuestion = () => {
+  let questions = [
+    new QuestionModel('Pourquoi ?')
+      .withAnswer({labelA: '1', labelB: '2'})
+      .withAnswer({labelA: '2', labelB: '3'})
+      .withRightAnswer(1)
+    ,
+    new QuestionModel('Pourquoi 2 ?')
+      .withAnswer({labelA: '1', labelB: '2'})
+      .withAnswer({labelA: '2', labelB: '3'})
+      .withRightAnswer(1)
+    ,
+  ];
+  question.value = questions[getRandomInt(questions.length)];
+}
+
+reinitialiserQuestion();
+
+
+
 </script>
 
 <template>
-  Game
-
-  <RunAwayInput />
-  <RunAwayButton />
+  <Question :question='question' @reset-questions="reinitialiserQuestion">
+  </Question>
 </template>
 
 <style scoped>
