0ab984706ac1d7a6c8cc1924f312295e21ca14aa
SamuelPouyade <samuel.pouyade@outlook.fr>
2024-12-06 01:37:46 +0100

    Ajout d'un modéle pour les questions
    Permet d'ajouter comment sera organisée
    la partie questions/réponse

---
+++ frontend/src/components/Question.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{
+  visible: boolean
+}>();
+</script>
+
+<template>
+  <Questions :open='visible' class='overflow-auto p-10 rounded-xl border-2 border-black'>
+    { questions }
+  </Questions>
+</template>
+
+<style scoped>
+
+</style>
