59d5c19576ad75f758ec953dd9027b888f0bff39
Paulloooo <115004751+Paulloooo@users.noreply.github.com>
2024-12-06 03:30:27 +0100

    pour erwan

--- frontend/src/components/Credits.vue
+++ frontend/src/components/Credits.vue
@@ -3,9 +3,34 @@
 </script>
 
 <template>
-  Credits
+  <div class="ocean">
+      <!-- Dauphin -->
+      <img
+        src="/src/assets/dolphin.png"
+        alt="Dolphin"   
+        class="dolphin"
+      />
+  </div>
 </template>
 
 <style scoped>
+/* Container principal pour l'océan */
+.ocean {
+  width: 100%;
+  height: 100vh;
+  background: linear-gradient(to bottom, #87ceeb, #005f99); /* Couleurs ciel et océan */
+  overflow: hidden;
+  position: relative;
+}
+
+
+.dolphin {
+  width: 100px;
+  position: absolute;
+  left: 50%;
+  bottom: 40%;
+  animation: rotate 2s infinite cubic-bezier(0,.5,.5,1);  
+}
+
 
 </style>
